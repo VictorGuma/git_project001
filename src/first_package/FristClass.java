3f7de146151f8c33d558024f5f6a1322714c916f
VictorGuma <victorguma18@gmail.com>
2020-08-08 21:04:49 +0300

    commitment

--- src/first_package/FristClass.java
+++ src/first_package/FristClass.java
@@ -5,7 +5,7 @@ public class FristClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Our first commit from local repo");
-		System.out.println("Our second commit from local repsitory");
+		System.out.println("Our second commit from local repository");
 		
 	}
 
